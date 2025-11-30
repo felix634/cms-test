5b48015e656aa8392b51a9d740e864f222db1fcf
Felix Bary <felixbary@gmail.com>
2025-11-30 18:41:49 +0100

    technicality

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import netlify from '@astrojs/netlify'; // 👈 CHECK THIS IMPORT
 
 export default defineConfig({
-  output: 'static',
+  output: 'server', // 👈 REQUIRED for the CMS
+  adapter: netlify(), // 👈 THIS IS MISSING IN YOUR ERROR
   integrations: [
     tailwind(), 
     react(), 
-    keystatic() // 👈 The magic line
+    keystatic()
   ],
 });
